6fd2336b1935661f443c2fff400363bc0c5970b6
Andrew Redinbo <atredinbo@gmail.com>
2019-12-08 02:29:03 -0500

    lemonbar working =D

--- roles/common/gui/files/config/bspwm/bar.sh
+++ roles/common/gui/files/config/bspwm/bar.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-Clock() {
+clock() {
     CURTIME=$(date "+%I:%M %p")
 
     echo -n "$CURTIME"
 }
 
+bspwm() {
+    BSPWM=$(bspc query -D)
+    BUSY=$(bspc query -D -d .occupied)
+
+    for DESK in $BSPWM
+    do
+        CHAR="\uf10c"
+        if [[ $BUSY =~ $DESK ]]
+        then
+            CHAR="\uf111"
+        fi
+        echo -n "%{A:bspc desktop $DESK -f:}"
+        if [ $DESK = $(bspc query -D -d) ]
+        then
+            echo -n "%{F#ccaa22}$CHAR%{F-}"
+        else
+            echo -n "$CHAR"
+        fi
+        echo -n "%{A} "
+    done
+}
+
 # Print
 while true; do
-        echo "%{c}%{F#FFFF00}%{B#0000FF} $(Clock) %{F-}%{B-}"
-        sleep 20
+    echo "%{l}$(bspwm)%{c}%{F#FFFF00}%{B#0000FF} $(date "+%I:%M %p") %{F-}%{B-}"
+    sleep 20
 done
